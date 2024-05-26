9c886b7fc6c94ca5139284e4f19e6c7388828c45
Gautam <mgautam2k2@gmail.com>
2024-05-26 23:57:37 +0530

    nav rail and screens

---
+++ lib/screens/movies.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoviesScreen extends StatelessWidget {
+  const MoviesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Movies');
+  }
+}
---
+++ lib/screens/shows.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShowsScreen extends StatelessWidget {
+  const ShowsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Shows');
+  }
+}
